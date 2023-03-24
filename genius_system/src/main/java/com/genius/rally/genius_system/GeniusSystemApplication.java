79c5e6353d9d0eefd2b939a554f1d0884429add4
yygy <2058443293@qq.com>
2023-03-24 11:28:32 +0800

    初始化

---
+++ genius_system/src/main/java/com/genius/rally/genius_system/GeniusSystemApplication.java
@@ -0,0 +1,13 @@
+package com.genius.rally.genius_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeniusSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GeniusSystemApplication.class, args);
+    }
+
+}
---
+++ genius_system/src/test/java/com/genius/rally/genius_system/GeniusSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.genius.rally.genius_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeniusSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
